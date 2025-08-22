8ffc5e4c5c7cb64251db17860a4039458fc2c4c9
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-08-22 16:07:15 -0500

    feat: introduce `Manual`-related .h/.cpp files to show usages of classes

---
+++ Source/Manual/Public/ManualExposed.h
@@ -0,0 +1,22 @@
+﻿//----------------------------------------------------------------------------------------------------
+// ManualExposed.h
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
+#pragma once
+
+#include <CoreMinimal.h>
+#include <ManualExposed.generated.h>
+
+//----------------------------------------------------------------------------------------------------
+UCLASS(Blueprintable)
+class MANUAL_API AManualExposed : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AManualExposed();
+
+	UFUNCTION(BlueprintCallable, Category = "Manual")
+	void DoManualExposed() const;
+};
