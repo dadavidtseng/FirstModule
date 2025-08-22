4c848e37095888390d3d965bbd03716346c5721d
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-08-22 12:27:19 -0500

    feat: initial commit

---
+++ Source/FirstModule/FirstModule.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstModule, "FirstModule" );
